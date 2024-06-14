74aacfdf074e06d54200af68d325fda337dc1f1c
loganthomasmusic <ltstewart95@gmail.com>
2024-06-14 11:13:20 -0400

    Create movie-view.jsx

---
+++ src/components/movie-view/movie-view.jsx
@@ -0,0 +1,49 @@
+import { useState } from "react";
+import { MovieCard } from "../movie-card/movie-card";
+import { MovieView } from "../movie-view/movie-view";
+
+export const MainView = () => {
+    const [movies, setMovies] = useState([
+        {
+            id: 1,
+            title: "Inception",
+            director: "Christopher Nolan",
+        },
+        {
+            id: 2,
+            title: "The Prestige",
+            director: "Christopher Nolan",
+        },
+        {
+            id: 3,
+            title: "The Dark Knight",
+            director: "Christopher Nolan",
+        },
+    ]);
+
+    const [selectedMovie, setSelectedMovie] = useState(null);
+
+    if (selectedMovie) {
+        return (
+            <MovieView movie={selectedMovie} onBackClick={() => setSelectedMovie(null)} />
+        );
+    }
+
+    if (movies.length === 0) {
+        return <div>The list is empty!</div>;
+    }
+
+    return (
+        <div>
+            {movies.map((book) => (
+                <MovieCard
+                    key={book.id}
+                    movie={movie}
+                    onMovieClick={(newSelectedMovie) => {
+                        setSelectedMovie(newSelectedMovie);
+                    }}
+                />
+            ))}
+        </div>
+    );
+};
