9d9ee98804414e0b15d30e3ca3022cbfffc01d59
loganthomasmusic <ltstewart95@gmail.com>
2024-06-14 11:14:00 -0400

    Create movie-card.jsx

---
+++ src/components/movie-card/movie-card.jsx
@@ -0,0 +1,11 @@
+export const MovieCard = ({ movie, onMovieClick }) => {
+    return (
+        <div
+            onClick={() => {
+                onMovieClick(book);
+            }}
+        >
+            {movie.title}
+        </div>
+    );
+};
