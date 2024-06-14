36201c1a19c4565c902855375ece3f5313164219
loganthomasmusic <ltstewart95@gmail.com>
2024-06-14 11:16:02 -0400

    Update movie-view.jsx

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,49 +1,18 @@
-import { useState } from "react";
-import { MovieCard } from "../movie-card/movie-card";
-import { MovieView } from "../movie-view/movie-view";
-
-export const MainView = () => {
-    const [movies, setMovies] = useState([
-        {
-            id: 1,
-            title: "Inception",
-            director: "Christopher Nolan",
-        },
-        {
-            id: 2,
-            title: "The Prestige",
-            director: "Christopher Nolan",
-        },
-        {
-            id: 3,
-            title: "The Dark Knight",
-            director: "Christopher Nolan",
-        },
-    ]);
-
-    const [selectedMovie, setSelectedMovie] = useState(null);
-
-    if (selectedMovie) {
-        return (
-            <MovieView movie={selectedMovie} onBackClick={() => setSelectedMovie(null)} />
-        );
-    }
-
-    if (movies.length === 0) {
-        return <div>The list is empty!</div>;
-    }
-
+export const MovieView = ({ movie, onBackClick }) => {
     return (
         <div>
-            {movies.map((book) => (
-                <MovieCard
-                    key={book.id}
-                    movie={movie}
-                    onMovieClick={(newSelectedMovie) => {
-                        setSelectedMovie(newSelectedMovie);
-                    }}
-                />
-            ))}
+            <div>
+                <img src={movie.image} />
+            </div>
+            <div>
+                <span>Title: </span>
+                <span>{movie.title}</span>
+            </div>
+            <div>
+                <span>Director: </span>
+                <span>{movie.director}</span>
+            </div>
+            <button onClick={onBackClick}>Back</button>
         </div>
     );
 };
