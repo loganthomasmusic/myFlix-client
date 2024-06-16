b8c429da3c375804cffe9b90791c9a6f0988b2d5
loganthomasmusic <ltstewart95@gmail.com>
2024-06-16 11:06:32 -0400

    Connect to API and add Prop Types

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,27 +1,21 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 
 export const MainView = () => {
-    const [movies, SetMovies] = useState([
-        {
-            id: 1,
-            title: "Inception",
-            director: "Christopher Nolan",
-        },
-        {
-            id: 2,
-            title: "The Prestige",
-            director: "Christopher Nolan",
-        },
-        {
-            id: 3,
-            title: "The Dark Knight",
-            director: "Christopher Nolan",
-        },
-    ]);
+    const [movies, setMovies] = useState([]);
 
     const [selectedMovie, setSelectedMovie] = useState(null);
+
+    useEffect(() => {
+        fetch("https://logan-movie-api-1dcba13d053e.herokuapp.com/movies")
+            .then((response) => response.json())
+            .then(movies => {
+                setMovies(movies)
+            })
+            .catch(e => console.log(e))
+
+    }, []);
 
     if (selectedMovie) {
         return (
