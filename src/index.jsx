014527ad0ce0c2eddea2a55ae9c8a2bdf93060be
loganthomasmusic <ltstewart95@gmail.com>
2024-07-04 11:07:17 -0400

    Implement Netlify

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,5 @@
+import Container from "react-bootstrap/Container";
 import { createRoot } from 'react-dom/client';
-
 import { MainView } from './components/main-view/main-view';
 
 // Import statement to indicate that you need to bundle `./index.scss`
